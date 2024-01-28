861c73178de5c4d28eae726e01fef88c8f8c6eaf
ProgramTantou <23051907@d.anabuki-college.net>
2024-01-28 15:14:20 +0900

    タイトル完成

--- Assets/lipiter/Script/Play.cs
+++ Assets/lipiter/Script/Play.cs
@@ -16,7 +16,7 @@ public class Play : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        PushL1 = Input.GetAxisRaw("P1L1");
+        PushL1 = Input.GetAxisRaw("P2L2");
     }
 
     private void OnTriggerStay(Collider other)
